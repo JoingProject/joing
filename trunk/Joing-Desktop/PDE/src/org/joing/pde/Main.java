c8e7e29fb21192031d7136bcec1694b28dcd40be
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2007-09-09 05:53:45 +0000

    Peyrona Desktop Environment (first commit).

---
+++ trunk/Joing-Desktop/PDE/src/org/joing/pde/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Main.java
+ *
+ * Created on 9 de septiembre de 2007, 7:51
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.joing.pde;
+
+/**
+ *
+ * @author Francisco Morero Peyrona
+ */
+public class Main
+{
+    
+    /** Creates a new instance of Main */
+    public Main()
+    {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
