6c1ade67da99a15b8eda94a851b53363039b80c3
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2007-11-16 09:13:22 +0000

    Moved remotely

---
+++ trunk/Joing-Client/src/org/joing/jvmm/RuntimeFactory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.joing.jvmm;
+
+import org.joing.common.clientAPI.jvmm.Platform;
+
+/**
+ * Una de las desiciones importantes aqui será determinar que implementación
+ * de Platform será la que se regresa. Esta desicion debera ser influida
+ * por ejemplo por factores como la plataforma (JavaSE, JavaME, etc).
+ * 
+ * @author antoniovl
+ */
+public class RuntimeFactory {
+
+    private static final Platform platform;
+
+    static {
+        platform = new PlatformImpl();
+    }
+    
+    public RuntimeFactory() {
+        if (platform == null) {
+            throw new RuntimeException("Platform was not initialized.");
+        }
+    }
+    
+    public static Platform getPlatform() {
+        return platform;
+    }
+}
