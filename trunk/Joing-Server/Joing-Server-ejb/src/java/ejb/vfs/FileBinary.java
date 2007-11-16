b55dad92011061d22b56815ac49683365ff1aa65
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2007-11-16 09:14:53 +0000

    Moved remotely

---
+++ trunk/Joing-Server/Joing-Server-ejb/src/java/ejb/vfs/FileBinary.java
@@ -0,0 +1,75 @@
+/*
+ * FileBinary.java
+ * 
+ * Created on 15-jul-2007, 11:44:38
+ * 
+ * Author: Francisco Morero Peyrona.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package ejb.vfs;
+
+import ejb.Constant;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+/**
+ * A subclass of FileDescritor that contains the native file contents 
+ * being the native file a binary file.
+ * 
+ * @author Francisco Morero Peyrona
+ */
+public class FileBinary extends FileDescriptor
+{
+    private byte[] btContent;
+    private String sMimeType;
+    
+    /**
+     * Class constructor (this class is a DTO).
+     * <p>
+     * For security and encapsulation reasons, the constructor has package scope:
+     * only the Manager EJB can create them.<br>
+     * If any other part of the application would need to create for example an
+     * empty instance of this class, then a method can be added to the Manager
+     * EJB (this method can return an empty instance).
+     */
+    FileBinary()
+    {
+    }
+
+    public InputStream getContent()
+    {
+        return new ByteArrayInputStream( btContent );
+    }
+    
+    public String getMimeType()
+    {
+        return sMimeType;
+    }
+    
+    //------------------------------------------------------------------------//
+    // PACKAGE SCOPE
+    
+    void setContents( byte[] btContent )
+    {
+        this.btContent = btContent;    // TODO: hacer copia defensiva
+    }
+    
+    // TODO: este metodo no se usa: mirar dónde usarlo
+    void setMimetipe( String sMimeType )
+    {
+        this.sMimeType = sMimeType;
+    }
+}
---
+++ trunk/Joing-Server/Joing-Server-war/src/java/servlets/session/Logout.java
@@ -0,0 +1,86 @@
+/*
+ * Logout.java
+ *
+ * Created on 30 de junio de 2007, 10:12
+ */
+
+package servlets.session;
+
+import ejb.session.SessionManagerLocal;
+import java.io.*;
+import java.net.*;
+import javax.ejb.EJB;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author fmorero
+ * @version
+ */
+public class Logout extends HttpServlet
+{
+    @EJB
+    private SessionManagerLocal sessionManagerBean;
+    
+    //------------------------------------------------------------------------//
+    
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void processRequest( HttpServletRequest request, HttpServletResponse response )
+              throws ServletException, IOException
+    {
+        ObjectInputStream reader  = new ObjectInputStream( request.getInputStream() );
+        
+        try
+        {
+            String sSessionId = (String) reader.readObject();
+         
+            sessionManagerBean.logout( sSessionId );   
+        }
+        catch( Exception exc )
+        {
+            // As logout(...) does not return this exception do not need to be reported (throw)
+            log( "Error in Servlet: "+ getClass().getName(), exc );
+        }
+        finally
+        {
+            if( reader != null )
+                try{ reader.close(); }catch( IOException exc ) { }
+        }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+    
+    /** Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+    
+    /** Returns a short description of the servlet.
+     */
+    public String getServletInfo()
+    {
+        return "Short description";
+    }
+    // </editor-fold>
+}
