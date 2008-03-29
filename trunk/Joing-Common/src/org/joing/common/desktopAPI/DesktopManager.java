d867953794df7ce5cb8e5b16fd01164aea4eb41b
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-03-29 11:23:20 +0000

    peyrona - Dublin version

--- trunk/Joing-Common/src/org/joing/common/desktopAPI/DesktopManager.java
+++ trunk/Joing-Common/src/org/joing/common/desktopAPI/DesktopManager.java
@@ -5,6 +5,7 @@
 
 package org.joing.common.desktopAPI;
 
+import java.awt.Image;
 import org.joing.common.desktopAPI.desktop.Desktop;
 
 /**
@@ -20,6 +21,23 @@ public interface DesktopManager
     // Called from Platform
     void shutdown();
     void halt();
+    /**
+     * This method is normally invoked from Platform to inform that a long task
+     * is in progress against Join'g Server.
+     * Every Joing client can implement this information as it would like: using
+     * a dialog, a mesagge in the status bar, etc.
+     * 
+     * @param sMesaage Message to be shown: if null an standard one will be used.
+     * @param icon     Icon to be shown: if null an standard one will be used.
+     * @return A handle to the message GUI to be closed when the trade is over.
+     */
+    int  startServerTradeInfo( String sMesaage, Image icon );
+    /**
+     * Hides an information GUI previously opended via startServerTradeInfo(...)
+     * 
+     * @param nHandle The one returned by startServerTradeInfo(...)
+     */
+    void stopServerTradeInfo( int nHandle );
     
     // References
     Desktop getDesktop();
