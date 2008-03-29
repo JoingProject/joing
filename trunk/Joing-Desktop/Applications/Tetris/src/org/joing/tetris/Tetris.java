b7a31fa9c1ec7217eca760ad64747e9f0a87a3d6
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-03-29 16:49:03 +0000

    peyrona - Dublin version

---
+++ trunk/Joing-Desktop/Applications/Tetris/src/org/joing/tetris/Tetris.java
@@ -0,0 +1,95 @@
+/*
+ * @(#)Main.java
+ *
+ * This work is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This work is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * Copyright (c) 2003 Per Cederberg. All rights reserved.
+ */
+
+package org.joing.tetris;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JInternalFrame;
+import javax.swing.event.InternalFrameAdapter;
+import org.joing.common.desktopAPI.DeskComponent;
+import org.joing.common.desktopAPI.DesktopManager;
+import org.joing.common.desktopAPI.pane.DeskFrame;
+
+/**
+ * The main class of the Tetris game. This class contains the necessary methods to run the game.
+ * 
+ * @version 1.2
+ * @author Per Cederberg, per@percederberg.net
+ */
+public class Tetris
+{
+    private Game game = null;   // The Tetris game being played.
+
+    public Tetris()
+    {
+        game = new Game();
+        showInFrame();
+    }
+    
+    //------------------------------------------------------------------------//
+    
+    private void showInFrame()
+    {
+        DesktopManager dm   = org.joing.jvmm.RuntimeFactory.getPlatform().getDesktopManager();
+        ImageIcon      icon = new ImageIcon( getClass().getResource( "tetris.png" ) );
+        
+        if( dm != null )
+        {
+            // Show this panel in a frame created by DesktopManager Runtime.
+            DeskFrame frame = dm.getRuntime().createFrame();
+                      frame.setTitle( "Pacman" );
+                      frame.setIcon( icon.getImage() );
+                      frame.add( (DeskComponent) game.getComponent() );
+                      ((JInternalFrame) frame).addInternalFrameListener( new InternalFrameAdapter()
+                         {
+                            public void internalFrameClosed( InternalFrameEvent e )
+                            {
+                                Tetris.this.game.quit();
+                            }
+                         } );
+            dm.getDesktop().getActiveWorkArea().add( frame );
+        }
+        else
+        {
+            javax.swing.JFrame frame = new javax.swing.JFrame();
+                               frame.add( game.getComponent() );
+                               frame.setIconImage( icon.getImage() );
+                               frame.pack();
+                               frame.setDefaultCloseOperation( javax.swing.JFrame.EXIT_ON_CLOSE );
+                               frame.setVisible( true );
+                               frame.addWindowListener( new WindowAdapter()
+                               {                
+                                   public void windowClosed( WindowEvent e )
+                                   {
+                                       Tetris.this.game.quit();
+                                   }
+                               } ); 
+        }
+    }
+    
+    /**
+     * The stand-alone main routine.
+     * 
+     * @param args the command-line arguments
+     */
+    public static void main( String[] args )
+    {
+        new Tetris();
+    }
+}
