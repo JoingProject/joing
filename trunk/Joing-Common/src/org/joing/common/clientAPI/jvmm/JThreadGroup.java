2ddfd6b2dd4a67510f1bf829c34b44a5c3cca04b
avarela <avarela@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-06-28 19:54:21 +0000

    EDT Thread invocation bugfix.

--- trunk/Joing-Common/src/org/joing/common/clientAPI/jvmm/JThreadGroup.java
+++ trunk/Joing-Common/src/org/joing/common/clientAPI/jvmm/JThreadGroup.java
@@ -6,20 +6,42 @@
 package org.joing.common.clientAPI.jvmm;
 
 import java.io.OutputStream;
+import java.util.UUID;
 
 /**
- *
- * @author antoniovl
+ * Thread Group Abstract Class.
+ * 
+ * @author Antonio Varela Lizardi <antonio@icon.net.mx>
  */
 public abstract class JThreadGroup extends ThreadGroup {
 
     protected Thread disposer;
 
-    /**
-     * Any override of this method should call super("..."),
-     */
     public JThreadGroup() {
-        super("JoingThreadGroup:" + System.nanoTime());
+        super("JoingThreadGroup_" + UUID.randomUUID().toString());
+    }
+    
+    public JThreadGroup(String name) {
+        super(name);
+    }
+    
+    public JThreadGroup(String name, Thread disposer, ThreadGroup parent) {
+        super(parent, name);
+        this.disposer = disposer;
+    }
+    
+    public JThreadGroup(String name, ThreadGroup parent) {
+        this(name, null, parent);
+    }
+    
+    public JThreadGroup(Thread disposer) {
+        this();
+        this.disposer = disposer;
+    }
+    
+    public JThreadGroup(Thread disposer, ThreadGroup parent) {
+        super(parent, "JoingThreadGroup " + UUID.randomUUID().toString());
+        this.disposer = disposer;
     }
 
     public void setDisposer(Thread disposer) {
@@ -31,7 +53,7 @@ public abstract class JThreadGroup extends ThreadGroup {
     public abstract OutputStream getErr();
 
     /**
-     * Closes this ThreadGroup calling the specific disposer thread.
+     * Closes this ThreadGroup calling the disposer thread.
      */
     public abstract void close();
     
