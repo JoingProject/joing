aba6ce7e6495e5a00ab7162107f454ef06432e6f
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-07-20 09:28:10 +0000

    Ready to start beta testing period (Summer 2008)

--- trunk/Joing-Desktop/Applications/Images/src/org/joing/images/StatusBar.java
+++ trunk/Joing-Desktop/Applications/Images/src/org/joing/images/StatusBar.java
@@ -1,10 +1,21 @@
-/*
- * (c) Telco Domótica S.L.
- * Todos los derechos reservados.
+/* 
+ * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
+ *
+ * This file is part of Join'g project: www.joing.org
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the free
+ * Software Foundation; either version 3, or (at your option) any later version.
  * 
- * Creado el 06-sep-2005 a las 23:11:22
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 package org.joing.images;
 
 import java.awt.Dimension;
@@ -18,6 +29,10 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
+/**
+ * 
+ * @author Francisco Morero Peyrona
+ */
 class StatusBar extends Box
 {
     private JLabel lblHelp, lblSize, lblRotation, lblScale;
--- trunk/Joing-Desktop/Applications/Images/src/org/joing/images/ToolBar.java
+++ trunk/Joing-Desktop/Applications/Images/src/org/joing/images/ToolBar.java
@@ -1,10 +1,21 @@
-/*
- * (c) Telco Domótica S.L.
- * Todos los derechos reservados.
+/* 
+ * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
+ *
+ * This file is part of Join'g project: www.joing.org
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the free
+ * Software Foundation; either version 3, or (at your option) any later version.
  * 
- * Creado el 11-sep-2005 a las 13:20:45
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 package org.joing.images;
 
 import java.awt.event.ActionEvent;
@@ -21,6 +32,10 @@ import javax.swing.JComponent;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
 
+/**
+ * 
+ * @author Francisco Morero Peyrona
+ */
 public class ToolBar extends JToolBar
 {
     private JButton rotateLeft  = createButton( new RotateLeft()  );
