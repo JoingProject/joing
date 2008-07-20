6084091a61ab57fdfe3e12548dcc981ed5225032
peyrona <peyrona@gmail.com>
2008-07-20 09:28:10 +0000

    Ready to start beta testing period (Summer 2008)

--- Joing-Desktop/Applications/Images/src/org/joing/images/WImage.java
+++ Joing-Desktop/Applications/Images/src/org/joing/images/WImage.java
@@ -1,10 +1,21 @@
-/*
- * (c) Telco Domótica S.L.
- * Todos los derechos reservados.
+/* 
+ * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
+ *
+ * This file is part of Join'g project: www.joing.org
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the free
+ * Software Foundation; either version 3, or (at your option) any later version.
  * 
- * Creado el 05-sep-2005 a las 20:16:50
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 package org.joing.images;
 
 import java.awt.Component;
@@ -17,6 +28,10 @@ import java.awt.geom.AffineTransform;
 import javax.swing.JComponent;
 import javax.swing.JViewport;
 
+/**
+ * 
+ * @author Francisco Morero Peyrona
+ */
 public class WImage extends JComponent
 {
     private Image     image       = null;
