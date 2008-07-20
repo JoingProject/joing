43bb83d9b78a55dd9733cd2fe2664603b92b7ac8
peyrona <peyrona@gmail.com>
2008-07-20 09:27:18 +0000

    Ready for beta testing period (Summer 2008)

--- Joing-Desktop/Applications/BasicCalculator/src/org/joing/basicalc/PaperPanel.java
+++ Joing-Desktop/Applications/BasicCalculator/src/org/joing/basicalc/PaperPanel.java
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
+ *
+ * This file is part of Join'g project: www.joing.org
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the free
+ * Software Foundation; either version 3, or (at your option) any later version.
+ * 
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.joing.basicalc;
 
 import java.awt.Color;
@@ -12,6 +30,10 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
+/**
+ * 
+ * @author Francisco Morero Peyrona
+ */
 final class PaperPanel extends JScrollPane
 {
     private Vector vLines   = new Vector();
