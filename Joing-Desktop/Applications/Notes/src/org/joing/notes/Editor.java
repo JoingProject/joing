038bb355117fa8f13ea69fa4094c6699e7a15cd8
peyrona <peyrona@gmail.com>
2008-07-20 09:28:56 +0000

    Ready to start beta testing period (Summer 2008)

--- Joing-Desktop/Applications/Notes/src/org/joing/notes/Editor.java
+++ Joing-Desktop/Applications/Notes/src/org/joing/notes/Editor.java
@@ -1,10 +1,21 @@
-/*
- * (c) Telco Domótica S.L.
- * Todos los derechos reservados.
+/* 
+ * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
+ *
+ * This file is part of Join'g project: www.joing.org
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the free
+ * Software Foundation; either version 3, or (at your option) any later version.
  * 
- * Creado el 05-sep-2005 a las 20:16:50
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 package org.joing.notes;
 
 import java.awt.Insets;
@@ -14,6 +25,10 @@ import javax.swing.event.UndoableEditEvent;
 import javax.swing.event.UndoableEditListener;
 import javax.swing.undo.UndoManager;
 
+/**
+ * 
+ * @author Francisco Morero Peyrona
+ */
 class Editor extends JTextArea
 {
     private UndoManager undomanager;
