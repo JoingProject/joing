c45edb9b6544a19e1902b8099d2b4a4a9698796f
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-07-20 09:28:56 +0000

    Ready to start beta testing period (Summer 2008)

--- trunk/Joing-Desktop/Applications/Notes/src/org/joing/notes/Notes.java
+++ trunk/Joing-Desktop/Applications/Notes/src/org/joing/notes/Notes.java
@@ -1,10 +1,21 @@
-/*
- * (c) Telco Domótica S.L.
- * Todos los derechos reservados.
+/* 
+ * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
+ *
+ * This file is part of Join'g project: www.joing.org
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the free
+ * Software Foundation; either version 3, or (at your option) any later version.
  * 
- * Creado el 03-sep-2005 a las 11:44:21
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 package org.joing.notes;
 
 import java.awt.BorderLayout;
@@ -45,7 +56,7 @@ import org.joing.common.desktopAPI.DesktopManager;
 import org.joing.common.desktopAPI.pane.DeskFrame;
 
 /**
- * A simple editor similar to Windows NotePad.
+ * A simple plain text editor similar to Windows NotePad.
  * 
  * @author Francisco Morero Peyrona
  */
@@ -61,7 +72,6 @@ public class Notes extends JPanel implements DeskComponent
     
     public Notes()
     {
-        JLabel label = new JLabel( "Hello World" );
         toolbar = new ToolBar();
         status  = new StatusBar();
         tabs    = new JTabbedPane();
