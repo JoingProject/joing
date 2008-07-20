10b70886a3066368339714d6074e7e3a8cd699f8
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-07-20 09:31:20 +0000

    Ready to start beta testing period (Summer 2008)

--- trunk/Joing-Desktop/Applications/YACE/src/org/joing/yace/YACE.java
+++ trunk/Joing-Desktop/Applications/YACE/src/org/joing/yace/YACE.java
@@ -1,7 +1,20 @@
-/*
- * YACE.java
+/* 
+ * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
  *
- * Created on 11 de julio de 2008, 16:06
+ * This file is part of Join'g project: www.joing.org
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the free
+ * Software Foundation; either version 3, or (at your option) any later version.
+ * 
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.joing.yace;
 
@@ -62,7 +75,7 @@ public class YACE extends javax.swing.JPanel implements DeskComponent
         {
             // Show this panel in a frame created by DesktopManager Runtime.
             DeskFrame frame = dm.getRuntime().createFrame();
-                      frame.setTitle( "Yet Another Community Explorer" );
+                      frame.setTitle( "Community Explorer (Mockup)" );
                       frame.setIcon( icon.getImage() );
                       frame.add( (DeskComponent) this );
                       
