5b07d627f5f8c469b5567096b1055b96c78e85fa
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-08-18 16:22:47 +0000

    Santiago edition: ready to start beta testing period (Summer 2008)

--- trunk/Joing-Desktop/Applications/SystemMonitor/src/org/joing/sysmon/SystemMonitor.java
+++ trunk/Joing-Desktop/Applications/SystemMonitor/src/org/joing/sysmon/SystemMonitor.java
@@ -1,5 +1,7 @@
-/* 
- * Copyright (C) 2007, 2008 Join'g Team Members.  All Rights Reserved.
+/*
+ * Copyright (C) 2007, 2008 Join'g Team Members. All Rights Reserved.
+ * Join'g Team Members are listed at project's home page. By the time of 
+ * writting this at: https://joing.dev.java.net/servlets/ProjectMemberList.
  *
  * This file is part of Join'g project: www.joing.org
  *
@@ -79,7 +81,7 @@ public class SystemMonitor extends JPanel implements DeskComponent
         DeskFrame frame = dm.getRuntime().createFrame();
                   frame.setTitle( "System Monitor" );
                   frame.setIcon( icon.getImage() );
-                  frame.add( (DeskComponent) this );
+                  frame.add( (DeskComponent) SystemMonitor.this );
 
         dm.getDesktop().getActiveWorkArea().add( frame );
     }
