e4f4175381f4ad6fb0a05fcf25e8bcd0de0c3420
peyrona <peyrona@5e6a94f9-e626-4eda-e9f3-bca8eefb053b>
2008-08-19 17:27:03 +0000

    Santiago edition: ready to start beta testing period (Summer 2008)

--- trunk/Joing-Desktop/Applications/Images/src/org/joing/images/WImage.java
+++ trunk/Joing-Desktop/Applications/Images/src/org/joing/images/WImage.java
@@ -18,7 +18,7 @@
  * GNU Classpath; see the file COPYING.  If not, write to the Free Software 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package org.joing.images.images;
+package org.joing.images;
 
 import java.awt.Component;
 import java.awt.Dimension;
