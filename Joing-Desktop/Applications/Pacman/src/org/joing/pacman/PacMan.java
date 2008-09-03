19b3f9dcea53e4759734ef460582396f08268600
peyrona <peyrona@gmail.com>
2008-09-03 16:56:50 +0000

    Santiago edition: ready to start beta testing period (Summer 2008)

--- Joing-Desktop/Applications/Pacman/src/org/joing/pacman/PacMan.java
+++ Joing-Desktop/Applications/Pacman/src/org/joing/pacman/PacMan.java
@@ -26,9 +26,9 @@ import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import javax.swing.event.InternalFrameAdapter;
 import javax.swing.event.InternalFrameEvent;
-import org.joing.common.desktopAPI.DeskComponent;
-import org.joing.common.desktopAPI.DesktopManager;
-import org.joing.common.desktopAPI.pane.DeskFrame;
+import org.joing.kernel.api.desktop.DeskComponent;
+import org.joing.kernel.api.desktop.DesktopManager;
+import org.joing.kernel.api.desktop.pane.DeskFrame;
 
 public class PacMan extends JPanel implements DeskComponent
 {
@@ -847,7 +847,7 @@ public class PacMan extends JPanel implements DeskComponent
     
     public void showInFrame()
     {
-        DesktopManager dm   = org.joing.jvmm.RuntimeFactory.getPlatform().getDesktopManager();
+        DesktopManager dm   = org.joing.kernel.jvmm.RuntimeFactory.getPlatform().getDesktopManager();
         ImageIcon      icon = new ImageIcon( getClass().getResource( "pacman.png" ) );
         
         if( dm != null )
